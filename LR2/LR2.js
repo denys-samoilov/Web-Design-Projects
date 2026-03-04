34e5499536397ab2bdb5fd75c1e3f8f3d1e117e9
Denys <133922338+denys-samoilov@users.noreply.github.com>
2026-03-04 08:42:48 +0200

    Added Seasons

--- LR2/LR2.js
+++ LR2/LR2.js
@@ -56,4 +56,39 @@ function task3() {
 
     alert("Оцінка: " + gradeString + "\nВідрахування: " + (grade < 50 ? "Так" : "Ні"));
     
+    let month = 12;
+
+    let monthString = "";
+
+    switch(month) {
+        case 12:
+        case 1:
+        case 2: {
+            monthString = "Winter";
+            break;
+        }
+        case 3:
+        case 4:
+        case 5: {
+            monthString = "Spring";
+            break;
+        }
+        case 6:
+        case 7:
+        case 8: {
+            monthString = "Summer";
+            break;
+        }
+        case 9:
+        case 10:
+        case 11: {
+            monthString = "Autumn";
+            break;
+        }
+        default: {
+            monthString = "Invalid month";
+            break;
+        }
+}
+    alert("Month " + month + ": " + monthString);
 }
